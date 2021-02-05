4a72ed2b8d0298320d862d5f9625d66069711682
Waxmatuct <alvheim@rambler.ru>
2021-02-05 12:31:34 +0300

    some adds

--- footer.php
+++ footer.php
@@ -1,17 +1,7 @@
 <footer class="blog-footer">
 	<p>© Редакционно-издательский центр, <? $date = date("Y"); echo $date; ?><br> ФГБОУ ВО «Сочинский государственный университет»</p>
-	<small>Дизайн и разработка сайта - <a href="mailto:pletnevsochi@yandex.ru">Дмитрий Плетнев</a></small>
-	<!-- <p>
-		<a href="#">Наверх</a> / 
-		<?php  if($_SESSION['admin'] != "admin") { ?>
-			<a href="login.php">Логин</a>
-		<? } 
-		else {
-			?>
-			<a href="?do=logout">Выйти</a>
-			<?}?>
-		</p> -->
-	</footer>
+	<small>Разработка сайта - <a href="mailto:pletnevsochi@yandex.ru">Дмитрий Плетнев</a></small>
+</footer>
 	<?php wp_footer(); ?>
 </body>
 </html>
